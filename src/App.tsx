27ce5153cc0fc0a7433a1ccf1dd9fa6b447f77e5
BGMarinho <bgmarinho96@gmail.com>
2024-08-18 15:27:11 -0300

    feature (App.tsx): Designing the most basic structure of the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 import './App.css';
 
 export default function App() {
-  return <main className="">teste</main>;
+  return (
+    <main>
+      <section className="search-bar">
+        <div className="wrapper">
+          <input type="text" name="" id="" placeholder="pokemon" />
+          <button>
+            <img src="" alt="pesquisa" />
+          </button>
+        </div>
+      </section>
+      <section className="pokemon-list">
+        <div className="wrapper">
+          <div className="card"></div>
+        </div>
+      </section>
+    </main>
+  );
 }

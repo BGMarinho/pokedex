eb645ee68767ee4b197ba1732a31046a028698cb
BGMarinho <bgmarinho96@gmail.com>
2024-08-20 14:22:17 -0300

    feature (pagination): starting the pagination system.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import './App.css';
+import searchIcon from './assets/search.svg';
+import leftArrow from './assets/left-arrow.svg';
+import rightArrow from './assets/right-arrow.svg';
 import { useState, useEffect } from 'react';
 
 interface Pokemon {
@@ -7,18 +10,19 @@ interface Pokemon {
 }
 
 export default function App() {
-  const BASE_URL = 'https://pokeapi.co/api/v2';
+  const BASE_URL = 'https://pokeapi.co/api/v2/';
   const [pokemonList, setPokemonList] = useState<Pokemon[]>();
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     const fetchPokemon = async () => {
-      await fetch(`${BASE_URL}/pokemon`)
+      // await fetch(`${BASE_URL}/pokemon?limit=${page * 20}`)
+      await fetch(`${BASE_URL}/pokemon?limit=20`)
         .then((response) => response.json())
         .then((response) => setPokemonList(response.results));
     };
-
     fetchPokemon();
-  }, []);
+  }, [page]);
 
   return (
     <main>
@@ -26,7 +30,7 @@ export default function App() {
         <div className="wrapper">
           <input type="text" name="" id="" placeholder="pokemon" />
           <button>
-            <img src="" alt="pesquisa" />
+            <img src={searchIcon} alt="pesquisa" width={16} height={16} />
           </button>
         </div>
       </section>
@@ -39,6 +43,7 @@ export default function App() {
                     <div className="sprite-wrapper">
                       <img
                         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+                        // src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${offset}.png`}
                         alt={pokemon.name}
                       />
                       <span>{pokemon.name}</span>
@@ -47,6 +52,15 @@ export default function App() {
                 );
               })
             : 'Carregando...'}
+          <div className="pagination">
+            <button disabled={page <= 1} onClick={() => setPage(page - 1)}>
+              <img src={leftArrow} alt="voltar" width={16} height={16} />
+            </button>
+            <p>{page}</p>
+            <button onClick={() => setPage(page + 1)}>
+              <img src={rightArrow} alt="avançar" width={16} height={16} />
+            </button>
+          </div>
         </div>
       </section>
     </main>

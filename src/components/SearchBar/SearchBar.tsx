9c879e8149b5b056af881af60b3a97a4ed8af49a
BGMarinho <bgmarinho96@gmail.com>
2024-08-21 12:27:18 -0300

    feature (components implementations): Implementing a handle function for onChange and an interface for the component props.

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,31 @@
 import searchIcon from '../../assets/search.svg';
+import { useState, useEffect } from 'react';
 
-interface SearchBarProps {}
+interface SearchBarProps {
+  searchText: string;
+  setSearchText: React.Dispatch<React.SetStateAction<string>>;
+}
 
-export default function SearchBar({}: SearchBarProps) {
+export default function SearchBar({
+  searchText,
+  setSearchText,
+}: SearchBarProps) {
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  useEffect(() => console.log(searchText), [searchText]);
+
   return (
     <div className="wrapper">
-      <input type="text" name="" id="" placeholder="pokemon" />
+      <select name="" id=""></select>
+
+      <input
+        type="text"
+        placeholder="number or name"
+        onChange={handleTextChange}
+      />
+
       <button>
         <img src={searchIcon} alt="pesquisa" width={16} height={16} />
       </button>

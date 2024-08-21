639834f1f628a3c60775b069c6f6c841f5a133e8
BGMarinho <bgmarinho96@gmail.com>
2024-08-21 17:05:46 -0300

    fix (PokemonList): Fixing the way that the promisses in the requisition are tretated.

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,31 +1,32 @@
 import Pagination from './Pagination';
 import { useState, useEffect } from 'react';
 
-interface Pokemon {
+interface IPokemon {
   name: string;
   url: string;
 }
 
-interface PokemonList extends Array<Pokemon> {}
+interface IPokemonList extends Array<IPokemon> {}
 
-interface PokemonListProps {}
+// interface PokemonListProps {}
 
-export default function PokemonList({}: PokemonListProps) {
-  const BASE_URL = 'https://pokeapi.co/api/v2/';
-  const [pokemonList, setPokemonList] = useState<PokemonList>();
+export default function PokemonList() {
+  const [pokemonList, setPokemonList] = useState<IPokemonList>();
   const [page, setPage] = useState(0);
 
   useEffect(() => {
     const fetchPokemonList = async () => {
-      await fetch(`${BASE_URL}/pokemon?limit=20&offset=${page * 20}`)
-        .then((response) => response.json())
-        .then((response) => setPokemonList(response.results));
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${page * 20}`,
+      );
+      const data = await response.json();
+      setPokemonList(data?.results);
     };
 
     fetchPokemonList();
   }, [page]);
 
-  const getPokemonPicture = (url: string) => {
+  const getPokemonPictureByURL = (url: string) => {
     const pokemon = url
       .replace('https://pokeapi.co/api/v2/pokemon/', '')
       .replace('/', '');
@@ -41,7 +42,7 @@ export default function PokemonList({}: PokemonListProps) {
                 <div key={index} className="card">
                   <div className="sprite-wrapper">
                     <img
-                      src={getPokemonPicture(pokemon.url)}
+                      src={getPokemonPictureByURL(pokemon.url)}
                       alt={pokemon.name}
                     />
                     <span>{pokemon.name}</span>
